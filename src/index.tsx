7ab5fa4357c1fde69720742275719c2d90ca8a4b
Daniil <dgilev75@gmail.com>
2025-06-25 14:04:04 +0300

    feat: remove extra files

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./components/App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Shop } from "@/pages/shop";
-import { LazyAbout } from "@/pages/about/About.lazy";
-import { Suspense } from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -11,20 +8,6 @@ const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
-        children: [
-            {
-                path: '/about',
-                element: <Suspense fallback={'Loading...'}>
-                    <LazyAbout />
-                </Suspense>
-            },
-            {
-                path: '/shop',
-                element: <Suspense fallback={'Loading...'}>
-                    <Shop />
-                </Suspense>
-            }
-        ]
     }
 ])
 
