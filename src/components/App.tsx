ec0a33fd55fe3521355023cfafd168ebfe48e874
Daniil <dgilev75@gmail.com>
2025-06-25 13:57:21 +0300

    feat: initialize project

---
+++ src/components/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+import * as styles from './App.module.scss';
+import { Link, Outlet } from 'react-router-dom';
+import A from '@/assets/icons8.png';
+import B from '@/assets/snapedit_1748871283906.jpg';
+import C from '@/assets/icons8.svg';
+
+export const App = () => {
+
+    const [count, setCount] = useState<number>(0);
+    const handleCount = () => setCount(prev => ++prev)
+
+    return (
+        <div>
+            <img src={A} alt="" />
+            <img src={B} alt="" width={100} height={100} />
+            <C color={'green'} width={100} height={100} />
+            <Link to={'/about'}>about</Link>
+            <Link to={'/shop'}>shop</Link>
+            <h1>{count}</h1>
+            <button className={styles.btn} onClick={handleCount}>count</button>
+            <Outlet />
+        </div>
+    )
+}
