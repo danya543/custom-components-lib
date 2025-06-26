c85e32f3d12f0d511baab341160e04d721aaadbb
Daniil <dgilev75@gmail.com>
2025-06-27 02:54:26 +0300

    feat: add eslint, prettier, husky Button

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,15 +1,23 @@
+import ESLintPlugin from 'eslint-webpack-plugin';
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import path from "path";
 import webpack from "webpack";
 import { Configuration } from "webpack";
-import { BuildOptions } from "./types/types";
-import path from "path";
 
-export function buildPlugins({ mode, paths, analyzer }: BuildOptions): Configuration['plugins'] {
+import { BuildOptions } from "./types/types";
+
+export function buildPlugins({ mode, paths }: BuildOptions): Configuration['plugins'] {
     const isDev = mode === 'development';
 
     const plugins: Configuration['plugins'] = [
-        new HtmlWebpackPlugin({ template: paths.html, favicon: path.resolve(paths.public, 'favicon.ico') })
+        new HtmlWebpackPlugin({ template: paths.html, favicon: path.resolve(paths.public, 'favicon.ico') }),
+        new ESLintPlugin({
+            extensions: ['ts', 'tsx'],
+            emitWarning: true,
+            emitError: true,
+            failOnError: false,
+        }),
     ]
     if (isDev) {
         plugins.push(new webpack.ProgressPlugin());
