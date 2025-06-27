01a55534727e8f20ee0c1482646ff4d15cdc8ee8
Daniil <dgilev75@gmail.com>
2025-06-27 03:15:30 +0300

    feat: add textfield

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,33 +1,93 @@
 import React from 'react';
+
 import { Button } from '../src/components/Button/Button';
+import { TextField } from '../src/components/TextField/TextField';
 import * as styles from './App.module.scss';
 
 export const App = () => {
+  const [value, setValue] = React.useState('');
+  const [value1, setValue1] = React.useState('');
 
-    return (
-        <div style={{ padding: '20px' }}>
-            <h1>Demo</h1>
-            <div className={styles.part}>
-                <h3>Buttons</h3>
-                <div className={styles.into_part}>
-                    <Button size="small" onClick={() => alert('contained')}>contained</Button>
-                    <Button size="medium" onClick={() => alert('contained')}>contained</Button>
-                    <Button size="large" onClick={() => alert('contained')}>contained</Button>
-                    <Button size="large" onClick={() => alert('contained')} disabled>contained</Button>
-                </div>
-                <div className={styles.into_part}>
-                    <Button size="small" onClick={() => alert('outlined')} variant='outlined' >outlined</Button>
-                    <Button size="medium" onClick={() => alert('outlined')} variant='outlined'>outlined</Button>
-                    <Button size="large" onClick={() => alert('outlined')} variant='outlined'>outlined</Button>
-                    <Button size="large" onClick={() => alert('outlined')} variant='outlined' disabled>outlined</Button>
-                </div>
-                <div className={styles.into_part}>
-                    <Button size="small" onClick={() => alert('text')} variant='text' >text</Button>
-                    <Button size="medium" onClick={() => alert('text')} variant='text'>text</Button>
-                    <Button size="large" onClick={() => alert('text')} variant='text'>text</Button>
-                    <Button size="large" onClick={() => alert('text')} variant='text' disabled>text</Button>
-                </div>
-            </div>
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Demo</h1>
+      <div className={styles.part}>
+        <h3>Buttons</h3>
+        <div className={styles.into_part}>
+          <Button size="small" onClick={() => alert('contained')}>
+            contained
+          </Button>
+          <Button size="medium" onClick={() => alert('contained')}>
+            contained
+          </Button>
+          <Button size="large" onClick={() => alert('contained')}>
+            contained
+          </Button>
+          <Button size="large" onClick={() => alert('contained')} disabled>
+            contained
+          </Button>
         </div>
-    );
+        <div className={styles.into_part}>
+          <Button
+            size="small"
+            onClick={() => alert('outlined')}
+            variant="outlined">
+            outlined
+          </Button>
+          <Button
+            size="medium"
+            onClick={() => alert('outlined')}
+            variant="outlined">
+            outlined
+          </Button>
+          <Button
+            size="large"
+            onClick={() => alert('outlined')}
+            variant="outlined">
+            outlined
+          </Button>
+          <Button
+            size="large"
+            onClick={() => alert('outlined')}
+            variant="outlined"
+            disabled>
+            outlined
+          </Button>
+        </div>
+        <div className={styles.into_part}>
+          <Button size="small" onClick={() => alert('text')} variant="text">
+            text
+          </Button>
+          <Button size="medium" onClick={() => alert('text')} variant="text">
+            text
+          </Button>
+          <Button size="large" onClick={() => alert('text')} variant="text">
+            text
+          </Button>
+          <Button
+            size="large"
+            onClick={() => alert('text')}
+            variant="text"
+            disabled>
+            text
+          </Button>
+        </div>
+
+        <div className={styles.part}>
+          <h3>TextFields</h3>
+          <TextField
+            value={value}
+            placeholder="Enter text"
+            onChange={e => setValue(e.target.value)}
+          />
+          <TextField
+            value={value1}
+            placeholder="Enter text1"
+            onChange={e => setValue1(e.target.value)}
+            error={true}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import * as styles from './TextField.module.scss';
+
+interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  value: string;
+  error?: boolean;
+}
+
+export const TextField: React.FC<TextFieldProps> = ({
+  value,
+  placeholder = '',
+  onChange,
+  className = '',
+  error = false,
+  ...rest
+}) => {
+  const [isFocused, setIsFocused] = React.useState(false);
+
+  return (
+    <div
+      className={`${styles.wrapper} ${className ? styles[className] : ''} ${error ? styles.error : ''}`}>
+      {(isFocused || value) && (
+        <label
+          className={`${styles.label} ${isFocused || value ? styles.labelFocused : ''} ${error ? styles.errorLabel : ''}`}>
+          {placeholder}
+        </label>
+      )}
+      <input
+        className={`${styles.textField}`}
+        value={value}
+        onChange={onChange}
+        placeholder={isFocused ? '' : placeholder}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        {...rest}
+      />
+    </div>
+  );
+};
