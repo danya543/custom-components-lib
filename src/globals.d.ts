d38475477ea84ec745926607f2ac9c9f37afc18e
Daniil <dgilev75@gmail.com>
2025-07-04 23:08:17 +0300

    feat: optimize using lib with other projects

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -10,7 +10,7 @@ declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.svg' {
-  import React from 'react';
-  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  import * as React from 'react';
+  const SVG: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   export default SVG;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Button } from './components/Button/Button';
 export { Checkbox } from './components/Checkbox/Checkbox';
+export { Modal } from './components/Modal/Modal';
 export { Select } from './components/Select/Select';
 export { Switch } from './components/Switch/Switch';
 export { TextField } from './components/TextField/TextField';
